d6de78b8af791c07d0e599795b2b07d996d480a0
Zeyad Habash <zeyad.habashy02@gmail.com>
2024-11-24 18:58:44 +0200

    add robot

---
+++ Assets/Scripts/GetPosition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPosition : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            Debug.Log(transform.position.ToString("F6"));
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
