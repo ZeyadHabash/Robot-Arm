160638b508f53656c44e80c6a65ecc007c4bd594
Zeyad Habash <zeyad.habashy02@gmail.com>
2024-11-26 00:14:35 +0200

    fix printing and finish forward posiiton

--- Assets/Scripts/GetPosition.cs
+++ Assets/Scripts/GetPosition.cs
@@ -8,8 +8,9 @@ public class GetPosition : MonoBehaviour
     {
         while (true)
         {
-            Debug.Log(transform.position.ToString("F6"));
-            yield return new WaitForSeconds(5);
+            Vector3 swappedPosition = new Vector3(transform.position.x, transform.position.z, transform.position.y);
+            Debug.LogFormat("Position: X = {0:F6}, Y = {1:F6}, Z = {2:F6}", swappedPosition.x, swappedPosition.y, swappedPosition.z);
+            yield return new WaitForSeconds(2);
         }
     }
 }
